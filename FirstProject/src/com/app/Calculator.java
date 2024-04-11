d90c0353f2979798f8d5e77abd9c8a996430331c
Nageswara <nageswara.nandimalla@gmail.com>
2024-04-11 12:43:46 -0400

    First Project commit

---
+++ FirstProject/src/com/app/Calculator.java
@@ -0,0 +1,14 @@
+package com.app;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+	
+		int num1=10;
+		int num2=20;
+		int result=num1+num2;
+		System.out.println(" Sum if num1+num2 => "+result);
+
+	}
+
+}
